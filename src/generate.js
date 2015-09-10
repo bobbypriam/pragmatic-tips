faa6aad1255e4572a026982cf41e41a136a2ecf4
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-10 10:27:55 +0700

    Initial commit

---
+++ src/generate.js
@@ -0,0 +1,11 @@
+import { TIPS } from './constants';
+
+function generateRandomTip() {
+  return {
+    // number: '1',
+    // title: 'Lorem Ipsum',
+    // description: 'Lorem ipsum dolor sit amet.'
+  };
+}
+
+export default generateRandomTip;
---
+++ test/test.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+
+var generate = require('../lib/generate');
+var TIPS = require('../lib/constants.js').TIPS;
+
+describe('pragmatic-tips', function () {
+
+  describe('generateRandomTip', function () {
+
+    it('should return an object containing number, title, and description', function () {
+      var tip = generate();
+
+      expect(tip).to.have.property('number');
+      expect(tip).to.have.property('title');
+      expect(tip).to.have.property('description');
+    });
+
+    it('should return one of the tip from TIPS constants', function () {
+      var tip = generate();
+
+      expect(TIPS).to.include(tip);
+    });
+
+  });
+
+});
