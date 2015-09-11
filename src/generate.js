16e60a9cf53ae353950bc281d504c6215e1fffa8
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-11 10:47:42 +0700

    Implement generate() and pass tests

--- src/generate.js
+++ src/generate.js
@@ -1,11 +1,19 @@
 import { TIPS } from './constants';
 
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ */
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function generate() {
-  return {
-    number: '1',
-    title: 'Lorem Ipsum',
-    description: 'Lorem ipsum dolor sit amet.'
-  };
+  const index = getRandomInteger(0, TIPS.length - 1);
+
+  let tip = TIPS[index];
+  tip.number = index + 1;
+
+  return tip;
 }
 
 export default generate;
